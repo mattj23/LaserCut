ff68e3ec4a694029c641ca6c9734e9990f4648bd
Matt Jarvis <mattj23@gmail.com>
2024-04-16 19:26:33 -0400

    line view model

---
+++ LaserCut.Avalonia/ViewModels/DrawViewModelBase.cs
@@ -0,0 +1,62 @@
+﻿using Avalonia.Media;
+using LaserCut.Geometry.Primitives;
+using ReactiveUI;
+
+namespace LaserCut.Avalonia.ViewModels;
+
+/// <summary>
+/// A base class for view models that represent a drawable object
+/// </summary>
+public abstract class DrawViewModelBase : ReactiveObject, IDrawViewModel
+{
+    protected double ZoomValue = 1;
+    protected Aabb2 BoundsValue;
+
+    private IBrush? _fill;
+    private IBrush? _stroke;
+    private double _strokeThickness;
+
+    protected DrawViewModelBase()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public Guid Id { get; }
+
+    public IBrush? Stroke
+    {
+        get => _stroke;
+        set => this.RaiseAndSetIfChanged(ref _stroke, value);
+    }
+
+    public IBrush? Fill
+    {
+        get => _fill;
+        set => this.RaiseAndSetIfChanged(ref _fill, value);
+    }
+
+    public double DisplayThickness => StrokeThickness / ZoomValue;
+
+    public void UpdateZoom(double zoom)
+    {
+        ZoomValue = zoom;
+        this.RaisePropertyChanged(nameof(DisplayThickness));
+    }
+
+    public Aabb2 Bounds
+    {
+        get => BoundsValue;
+        set => this.RaiseAndSetIfChanged(ref BoundsValue, value);
+    }
+
+    public double StrokeThickness
+    {
+        get => _strokeThickness;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _strokeThickness, value);
+            this.RaisePropertyChanged(nameof(DisplayThickness));
+        }
+    }
+
+}
---
+++ LaserCut.Avalonia/ViewModels/LineViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Avalonia;
+using Avalonia.Collections;
+using Avalonia.Media;
+using LaserCut.Geometry.Primitives;
+using ReactiveUI;
+
+namespace LaserCut.Avalonia.ViewModels;
+
+public class LineViewModel : DrawViewModelBase
+{
+    private Point _start;
+    private Point _end;
+    private AvaloniaList<double>? _dashArray;
+    
+    public LineViewModel() : base() {}
+    
+    public Point Start
+    {
+        get => _start;
+        set => this.RaiseAndSetIfChanged(ref _start, value);
+    }
+    
+    public Point End
+    {
+        get => _end;
+        set => this.RaiseAndSetIfChanged(ref _end, value);
+    }
+    
+    public AvaloniaList<double>? DashArray
+    {
+        get => _dashArray;
+        set => this.RaiseAndSetIfChanged(ref _dashArray, value);
+    }
+
+}
