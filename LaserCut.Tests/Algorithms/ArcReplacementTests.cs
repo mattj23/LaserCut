dfb11b69e9a20a541ce4ea05729b92e28f3e19d2
Matt Jarvis <mattj23@gmail.com>
2024-07-24 22:54:20 -0400

    simple arc replacement works

---
+++ LaserCut.Tests/Algorithms/ArcReplacementTests.cs
@@ -0,0 +1,62 @@
+﻿using LaserCut.Algorithms;
+using LaserCut.Geometry;
+using LaserCut.Tests.Helpers;
+using MathNet.Spatial.Euclidean;
+
+namespace LaserCut.Tests.Algorithms;
+
+public class ArcReplacementTests
+{
+    [Fact]
+    public void SimpleArcReplacement()
+    {
+        var loop = ToSegments(HalfCircle());
+        
+        loop.ReplaceLinesWithArcs(4, 0.01);
+        
+        Assert.Equal(2, loop.Count);
+        Assert.True(loop.Head is BoundaryLine);
+        Assert.True(loop.Tail is BoundaryArc);
+        Assert.Equal(Point2D.Origin, (loop.Tail as BoundaryArc)!.Center, PointCheck.Default);
+        Assert.Equal(new Point2D(0, -1), (loop.Tail as BoundaryArc)!.Point, PointCheck.Default);
+    }
+
+    [Fact]
+    public void FullCircleReplacement()
+    {
+        var loop = ToSegments(BoundaryLoop.Circle(0, 0, 1.0));
+
+    }
+
+    private BoundaryLoop HalfCircle()
+    {
+        var loop = new BoundaryLoop();
+        var cursor = loop.GetCursor();
+        cursor.ArcAbs(0, -1, 0, 0, false);
+        cursor.SegAbs(0, 1);
+        return loop;
+    }
+
+    private BoundaryLoop ToSegments(BoundaryLoop loop)
+    {
+        var working = new BoundaryLoop();
+        var write = working.GetCursor();
+
+        foreach (var e in loop.Elements)
+        {
+            var l = 0.0;
+            while (l < e.Length)
+            {
+                var p = e.AtLength(l).Point;
+                write.SegAbs(p.X, p.Y);
+                l += 0.02;
+            }
+            write.SegAbs(e.End.X, e.End.Y);
+        }
+        
+        working.RemoveZeroLengthElements();
+        working.RemoveAdjacentRedundancies();
+
+        return working;
+    }
+}
--- LaserCut/Algorithms/ReplaceWithArcs.cs
+++ LaserCut/Algorithms/ReplaceWithArcs.cs
@@ -10,7 +10,7 @@ namespace LaserCut.Algorithms;
 /// of n or more points can be replaced with an arc. This is useful for reducing the number of points in a path
 /// and getting true arcs in the boundary.
 /// </summary>
-internal static class ReplaceWithArcs
+public static class ReplaceWithArcs
 {
 
     /// <summary>
@@ -35,120 +35,101 @@ internal static class ReplaceWithArcs
         var dc = seg.PointAt(closest);
         return Math.Max(d0, circle.DistanceFromEdge(dc));
     }
-    
-    private static int[] NodeIds(BoundaryLoop loop, IBoundaryLoopCursor cursor, Interval interval)
+
+    private static BoundaryPoint[] PointsAt(BoundaryLoop loop, int start, int n)
     {
-        var start = cursor.GetIdBack(interval.Back);
-        var end = cursor.GetIdFwd(interval.Forward);
-        var results = new List<int>();
-        var working = loop.GetCursor(start);
-        while (working.CurrentId != loop.NextId(end))
+        var cursor = loop.GetCursor(start);
+        var results = new BoundaryPoint[n];
+        for (int i = 0; i < n; i++)
         {
-            results.Add(working.CurrentId);
-            working.MoveForward();
+            results[i] = cursor.Current;
+            cursor.MoveForward();
         }
 
-        return results.ToArray();
+        return results;
     }
     
-    private static (List<Point2D>, Circle2) ArcFromPoints(BoundaryLoop loop, IBoundaryLoopCursor c, Interval interval)
+    private static Circle2? CircleFromPoints(BoundaryPoint[] points)
     {
-        var points = new List<Point2D>();
-        foreach (var nodeId in NodeIds(loop, c, interval))
+        var p0 = points[0].Point;
+        var p1 = points[points.Length / 2].Point;
+        var p2 = points[^1].Point;
+        try
         {
-            // TODO: This check shouldn't be applied to the last point in the list
-            if (loop.ItemAt(nodeId) is BoundaryLine line)
-            {
-                points.Add(line.Point);
-            }
-            else
-            {
-                throw new ArgumentException("Invalid node type");
-            }
+            return new Circle2(p0, p1, p2);
         }
+        catch (ArgumentException)
+        {
+            return null;
+        }
+    }
+
+    private static bool ArcMatches(BoundaryLoop loop, int start, int n, double tol)
+    {
+        var points = PointsAt(loop, start, n);
+        if (points.SkipLast(1).Any(p => p is not BoundaryLine)) return false;
+        var circle = CircleFromPoints(points);
+        if (circle is null) return false;
         
-        // Create a 3-point circle
-        int middle = points.Count / 2;
-        var circle = new Circle2(points[0], points[middle], points[^1]);
-        return (points, circle);
-    }
-
-    private static bool ArcCheck(BoundaryLoop loop, IBoundaryLoopCursor c, Interval interval, double tolerance)
-    {
-        var (points, circle) = ArcFromPoints(loop, c, interval);
         var maxError = double.MinValue;
-        for (int i = 0; i < points.Count - 1; i++)
+        for (int i = 0; i < points.Length - 1; i++)
         {
-            var error = Farthest(circle, points[i], points[i + 1]);
+            var error = Farthest(circle, points[i].Point, points[i + 1].Point);
             maxError = Math.Max(maxError, error);
         }
-        Console.WriteLine(maxError);
 
-        return maxError <= tolerance;
-    }
-
-    private record Interval(int Back, int Forward)
-    {
-        public Interval ExpandBack() => this with { Back = Back - 1 };
-        public Interval ExpandForward() => this with { Forward = Forward + 1 };
-        
-        public int Length => Forward - Back;
-    };
-
-    private static Interval? LargestInterval(BoundaryLoop loop, IBoundaryLoopCursor cursor, int minPoints, double tolerance)
-    {
-        var interval = new Interval(0, minPoints);
-        if (ArcCheck(loop, cursor, interval, tolerance))
-        {
-            while (ArcCheck(loop, cursor, interval.ExpandForward(), tolerance) && interval.Length < loop.Count) 
-                interval = interval.ExpandForward();
-
-            while (ArcCheck(loop, cursor, interval.ExpandBack(), tolerance) && interval.Length < loop.Count) 
-                interval = interval.ExpandBack();
-            
-            return interval;
-        }
-
-        return null;
+        return maxError <= tol;
     }
     
-    public static void ReplaceLinesWithArcs(this BoundaryLoop loop, int minPoints, double tolerance)
+    public static void ReplaceLinesWithArcs(this BoundaryLoop loop, int minPoints, double tol)
     {
         if (minPoints < 4) throw new ArgumentException("Minimum number of points must be at least 4");
-        
-        // To make this work, we'll start at a point and look for a set of points that are within the tolerance for
-        // replacement with an arc.  If we find one, we'll check if expanding forward and backwards from the current
-        // set will allow us to replace more.  Once we have a set of points which can't be further expanded, we'll 
-        // perform the replacement.  We will continue this process until we've moved through the entire loop.
 
         var visited = new HashSet<int>();
-        var cursor = loop.GetCursor();
+        var walk = loop.GetCursor();
 
-        while (loop.Count > 3 && !visited.Contains(cursor.CurrentId))
+        while (loop.Count > 3 && !visited.Contains(walk.CurrentId))
         {
-            if (LargestInterval(loop, cursor, minPoints, tolerance) is { } interval)
+            var n = minPoints;
+            if (!ArcMatches(loop, walk.CurrentId, n, tol))
             {
-                visited.Clear();
-                var (points, circle) = ArcFromPoints(loop, cursor, interval);
-                // Are the points going clockwise or counter-clockwise?
-                var v0 = points[0] - circle.Center;
-                var v1 = points[1] - circle.Center;
-                bool clockwise = v0.CrossProduct(v1) < 0;
-                
-                // Get the ids of the nodes we're going to remove, skipping the last once
-                var ids = NodeIds(loop, cursor, interval).SkipLast(1).ToArray();
-                
-                cursor.MoveTo(ids[^1]);
-                while (ids.Contains(cursor.CurrentId)) cursor.Remove();
-                cursor.ArcAbs(points[0].X, points[0].Y, circle.Center.X, circle.Center.Y, clockwise);
+                visited.Add(walk.CurrentId);
+                walk.MoveForward();
+                continue;
             }
-            else
+            
+            while (ArcMatches(loop, walk.PreviousId, n + 1, tol) && n < loop.Count)
             {
-                visited.Add(cursor.CurrentId);
-                cursor.MoveForward();
+                walk.MoveBackward();
+                n++;
             }
-        }
+            
+            while (ArcMatches(loop, walk.CurrentId, n + 1, tol) && n < loop.Count)
+            {
+                if (n == 157)
+                {
+                    Console.WriteLine("here");
+                }
+                n++;
+            }
+            
+            var points = PointsAt(loop, walk.CurrentId, n);
+            var circle = CircleFromPoints(points);
+            if (circle is null) throw new InvalidOperationException("Invalid circle");
 
+            var original = walk.Current.Point;
+            
+            for (int k = 0; k < n - 1; k++)
+            {
+                walk.Remove();
+            }
+            
+            // Are the points going clockwise or counter-clockwise?
+            var v0 = points[0].Point - circle.Center;
+            var v1 = points[1].Point - circle.Center;
+            bool clockwise = v0.CrossProduct(v1) < 0;
+            walk.ArcAbs(original.X, original.Y, circle.Center.X, circle.Center.Y, clockwise);
+        }
     }
     
 }
