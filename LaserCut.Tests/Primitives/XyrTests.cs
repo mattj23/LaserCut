777a45d102836ad02fdc1cbd9562ed1ddc41f337
Matt Jarvis <mattj23@gmail.com>
2024-08-01 11:15:42 -0400

    xyr and unit tests

---
+++ LaserCut.Tests/Primitives/XyrTests.cs
@@ -0,0 +1,80 @@
+﻿using LaserCut.Geometry;
+using LaserCut.Tests.Helpers;
+using MathNet.Spatial.Euclidean;
+
+namespace LaserCut.Tests.Primitives;
+
+public class XyrTests
+{
+    [Fact]
+    public void DefaultIsEmpty()
+    {
+        var xyr = default(Xyr);
+        Assert.Equal(0, xyr.X, 1e-12);
+        Assert.Equal(0, xyr.Y, 1e-12);
+        Assert.Equal(0, xyr.R, 1e-12);
+    }
+
+    [Fact]
+    public void SimpleMove()
+    {
+        var xyr = new Xyr(1, 2, Math.PI / 2);
+        var point = new Point2D(0, 1);
+        var result = point.Transformed(xyr.AsMatrix());
+
+        var expected = new Point2D(0, 2);
+        
+        Assert.Equal(expected, result, PointCheck.Default);
+    }
+    
+    [Fact]
+    public void RoundTrip()
+    {
+        var xyr = new Xyr(1, 2, Math.PI / 2);
+        var matrix = xyr.AsMatrix();
+        var result = Xyr.FromMatrix(matrix);
+        
+        Assert.Equal(xyr, result, XyrCheck.Default);
+    }
+
+    [Fact]
+    public void StressTestRoundTrip()
+    {
+        var r = new RandomValues();
+        for (var i = 0; i < 10000; i++)
+        {
+            var xyr = new Xyr(r.Double(5), r.Double(5), r.Double(-Math.PI, Math.PI));
+            var matrix = xyr.AsMatrix();
+            var result = Xyr.FromMatrix(matrix);
+            
+            Assert.Equal(xyr, result, XyrCheck.Default);
+        }
+    }
+    
+}
+
+public class XyrCheck : IEqualityComparer<Xyr>
+{
+    private readonly double _tolerance;
+
+    public XyrCheck(double tolerance)
+    {
+        _tolerance = tolerance;
+    }
+
+    public bool Equals(Xyr x, Xyr y)
+    {
+        return Math.Abs(x.X - y.X) < _tolerance &&
+               Math.Abs(x.Y - y.Y) < _tolerance &&
+               Math.Abs(x.R - y.R) < _tolerance;
+    }
+
+    public int GetHashCode(Xyr obj)
+    {
+        return HashCode.Combine(obj.X, obj.Y, obj.R);
+    }
+    
+    public static XyrCheck Tol(double tolerance) => new(tolerance);
+
+    public static XyrCheck Default => Tol(GeometryConstants.DistEquals);
+}
--- LaserCut/Geometry/Xyr.cs
+++ LaserCut/Geometry/Xyr.cs
@@ -1,3 +1,33 @@
-﻿namespace LaserCut.Geometry;
+﻿using MathNet.Numerics.LinearAlgebra.Double;
 
-public readonly record struct Xyr(double X, double Y, double R);
+namespace LaserCut.Geometry;
+
+/// <summary>
+/// A struct describing the three components of a 2D rigid body transformation, X and Y translation and R rotation. The
+/// rotation value is in radians.
+/// </summary>
+/// <param name="X">Translation in X</param>
+/// <param name="Y">Translation in Y</param>
+/// <param name="R">Rotation, in radians, in the counter-clockwise direction</param>
+public readonly record struct Xyr(double X, double Y, double R)
+{
+    public Matrix AsMatrix() 
+    {
+        var cos = Math.Cos(R);
+        var sin = Math.Sin(R);
+        return DenseMatrix.OfArray(new[,]
+        {
+            {cos, -sin, X},
+            {sin, cos, Y},
+            {0, 0, 1}
+        });
+    }
+    
+    public static Xyr FromMatrix(Matrix m)
+    {
+        var x = m[0, 2];
+        var y = m[1, 2];
+        var r = Math.Atan2(m[1, 0], m[0, 0]);
+        return new Xyr(x, y, r);
+    }
+}
