eb2f179bf3c687c92b157c45a6538bdbe04ebdaa
Matt Jarvis <mattj23@gmail.com>
2024-08-04 17:59:10 -0400

    functioning origin manager

--- LaserCut.Avalonia/Models/OriginManager.cs
+++ LaserCut.Avalonia/Models/OriginManager.cs
@@ -1,48 +1,120 @@
 ﻿using System.Collections.ObjectModel;
 using DynamicData;
 using DynamicData.Binding;
+using ReactiveUI;
 
 namespace LaserCut.Avalonia.Models;
 
-public class OriginOptions
+public class OriginOptions : ReactiveObject
 {
-    private readonly ReadOnlyObservableCollection<IEntityWithOrigin> _options;
+    private readonly ObservableCollection<IEntityWithOrigin> _options;
 
-    public OriginOptions(ObservableCollection<IEntityWithOrigin> source, Guid excludeId)
+    public OriginOptions(IEnumerable<IEntityWithOrigin> source)
     {
-        source.ToObservableChangeSet()
-            .Filter(x => x.Origin.Id != excludeId)
-            .Bind(out _options)
-            .Subscribe();
+        _options = new ObservableCollection<IEntityWithOrigin>(source);
+        Options = new ReadOnlyObservableCollection<IEntityWithOrigin>(_options);
     }
     
-    public ReadOnlyObservableCollection<IEntityWithOrigin> Options => _options;
+    public void SynchronizeOptions(IEnumerable<IEntityWithOrigin> source)
+    {
+        var target = source.ToArray();
+        // Remove items that are no longer in the source, working backwards
+        for (int i = _options.Count - 1; i >= 0; i--)
+        {
+            if (!target.Contains(_options[i]))
+            {
+                _options.RemoveAt(i);
+            }
+        }
+        
+        // Add items that are in the source but not in the target
+        foreach (var item in target)
+        {
+            if (!_options.Contains(item))
+            {
+                _options.Add(item);
+            }
+        }
+    }
+
+    public ReadOnlyObservableCollection<IEntityWithOrigin> Options { get; }
+    
 }
 
 public class OriginManager
 {
     private readonly ObservableCollection<IEntityWithOrigin> _options = new();
+    private readonly Dictionary<Guid, HashSet<Guid>> _dependencies = new();
+    private readonly Dictionary<Guid, IOrigin> _origins = new();
+    private readonly Dictionary<Guid, OriginOptions> _filtered = new();
+    
     public OriginManager()
     {
-        // Options = new ReadOnlyObservableCollection<IEntityWithOrigin>(_options);
-        
-        _options.Add(new WorkspaceOrigin());
+        Add(new WorkspaceOrigin());
     }
-    
-    public ObservableCollection<IEntityWithOrigin> Options => _options;
     
     public void Add(IEntityWithOrigin entity)
     {
         _options.Add(entity);
+        _origins[entity.Origin.Id] = entity.Origin;
+        entity.Origin.ParentChanged
+            .Subscribe(_ => ConstructDependencies());
+        ConstructDependencies();
     }
     
     public void Remove(IEntityWithOrigin entity)
     {
         _options.Remove(entity);
+        _origins.Remove(entity.Origin.Id);
+        ConstructDependencies();
     }
 
-    public OriginOptions Filtered(Guid excludeId)
+    public OriginOptions Filtered(Guid itemId)
     {
-        return new OriginOptions(_options, excludeId);
+        if (_filtered.TryGetValue(itemId, out var filtered))
+        {
+            return filtered;
+        }
+        if (_dependencies.TryGetValue(itemId, out var deps))
+        {
+            _filtered[itemId] = new OriginOptions(_options.Where(o => !deps.Contains(o.Origin.Id)));
+        }
+        else
+        {
+            _filtered[itemId] = new OriginOptions([_options[0]]);
+        }
+        return _filtered[itemId];
+    }
+
+    private void ConstructDependencies()
+    {
+        _dependencies.Clear();
+        foreach (var option in _options)
+        {
+            AddChild(option.Origin.Id, option.Origin.Id);
+
+            var working = option.Origin;
+            while (working.ParentId != Guid.Empty)
+            {
+                AddChild(working.ParentId, option.Origin.Id);
+                working = _origins[working.ParentId];
+            }
+        }
+
+        foreach (var (itemId, options) in _filtered)
+        {
+            options.SynchronizeOptions(_options.Where(o => !_dependencies[itemId].Contains(o.Origin.Id)));
+        }
+    }
+    
+    private void AddChild(Guid parentId, Guid childId)
+    {
+        // TODO: this is naive and repetitive, fix it later
+        if (!_dependencies.ContainsKey(parentId))
+        {
+            _dependencies[parentId] = new HashSet<Guid>();
+        }
+        
+        _dependencies[parentId].Add(childId);
     }
 }
