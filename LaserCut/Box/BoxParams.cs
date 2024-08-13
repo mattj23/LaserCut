3db5e86dbbabb72095254c17c7520fcd3bb2d889
Matt Jarvis <mattj23@gmail.com>
2024-08-13 11:33:37 -0400

    started box geometry

---
+++ LaserCut/Box/BoxParams.cs
@@ -0,0 +1,12 @@
+namespace LaserCut.Box;
+
+public class BoxParams
+{
+    public double Width { get; set; }
+    public double Height { get; set; }
+    public double Length { get; set; }
+    
+    public double Thickness { get; set; }
+    
+    public double BaseInset { get; set; }
+}
